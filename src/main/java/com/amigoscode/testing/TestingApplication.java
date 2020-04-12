c3e4a1a584af13628d1d3730f717f13b1fabc711
Nelson Djalo <iamdjalas@gmail.com>
2020-04-12 15:02:43 +0100

    testing business logic

---
+++ src/main/java/com/amigoscode/testing/TestingApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
@@ -0,0 +1,39 @@
+package com.amigoscode.testing.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class CustomerRegistrationService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerRegistrationService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public void registerNewCustomer(CustomerRegistrationRequest request) {
+        String phoneNumber = request.getCustomer().getPhoneNumber();
+
+        Optional<Customer> customerOptional = customerRepository
+                .selectCustomerByPhoneNumber(phoneNumber);
+
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            if (customer.getName().equals(request.getCustomer().getName())) {
+                return;
+            }
+            throw new IllegalStateException(String.format("phone number [%s] is taken", phoneNumber));
+        }
+
+        if(request.getCustomer().getId() == null) {
+            request.getCustomer().setId(UUID.randomUUID());
+        }
+
+        customerRepository.save(request.getCustomer());
+    }
+}
