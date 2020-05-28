8d2b3b76bc335af316c18604daeb11d2c4e006e6
amigoscode <amigoscode@gmail.com>
2020-05-28 14:25:54 +0100

    test

--- src/main/java/com/amigoscode/testing/customer/CustomerRegistrationController.java
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("api/v1/customer-registration")
 public class CustomerRegistrationController {
